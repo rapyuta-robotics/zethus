5d06c48c52cbda07e395d5a10902a9be5aaddc37
Deep <chaitanya.deep@rapyuta-robotics.com>
2019-08-24 11:32:23 +0530

    Added support for pointstamped

---
+++ src/panels/sidebar/vizOptions/point.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { DEFAULT_OPTIONS_POINT } from 'amphion/src/utils/constants';
+import OptionRow from '../../../components/optionRow';
+import { updateOptionsUtil } from '../../../utils';
+import { Input } from '../../../components/styled';
+
+class PointOptions extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.updateOptions = updateOptionsUtil.bind(this);
+  }
+
+  render() {
+    const { options: propsOptions } = this.props;
+    const { alpha, color, radius } = {
+      ...DEFAULT_OPTIONS_POINT,
+      ...propsOptions,
+    };
+    return (
+      <React.Fragment>
+        <OptionRow label="Radius">
+          <Input
+            type="number"
+            name="radius"
+            step="0.01"
+            data-id="radius"
+            value={radius}
+            onChange={this.updateOptions}
+          />
+        </OptionRow>
+        <OptionRow label="Color">
+          <Input
+            type="color"
+            name="color"
+            data-id="color"
+            value={color}
+            onChange={this.updateOptions}
+          />
+        </OptionRow>
+        <OptionRow label="Alpha">
+          <Input
+            type="number"
+            name="alpha"
+            step="0.01"
+            data-id="alpha"
+            value={alpha}
+            onChange={this.updateOptions}
+          />
+        </OptionRow>
+      </React.Fragment>
+    );
+  }
+}
+
+export default PointOptions;
