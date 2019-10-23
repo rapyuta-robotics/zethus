7e96232eec48a7e670acb29b7fcf73a9b54a5f51
Deep <chaitanya.deep@rapyuta-robotics.com>
2019-10-23 11:54:26 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zethus/precache-manifest.022cb14e167126ca2636418287befade.js"
+  "/zethus/precache-manifest.dbd86f35e7f9167ec1eb9732fba59b3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zethus/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
