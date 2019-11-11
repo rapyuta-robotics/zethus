46fb6ae3ca6dcfc7eb681cc3c4a649dadedc4516
Deep <chaitanya.deep@rapyuta-robotics.com>
2019-11-11 21:25:14 +0530

    Added download button for config. Handle drag drop of config file in editor.

--- src/panels/configurationModal/index.jsx
+++ src/panels/configurationModal/index.jsx
@@ -6,7 +6,11 @@ import ace from 'brace';
 import 'brace/mode/json';
 import 'brace/theme/xcode';
 
-import { ButtonPrimary, FlexGrow } from '../../components/styled';
+import {
+  ButtonPrimary,
+  ButtonOutline,
+  FlexGrow,
+} from '../../components/styled';
 
 import {
   ModalActions,
@@ -14,28 +18,19 @@ import {
   ModalContents,
   ModalTitle,
 } from '../../components/styled/modal';
-import {
-  COLOR_GREY_LIGHT_1,
-  COLOR_GREY_LIGHT_2,
-  COLOR_PRIMARY,
-} from '../../components/styled/constants';
+import { COLOR_GREY_LIGHT_1 } from '../../components/styled/constants';
 import { stopPropagation } from '../../utils';
 
 const StyledEditor = styled.div`
   flex-grow: 1;
   flex-shrink: 1;
   background-color: ${COLOR_GREY_LIGHT_1};
-  .jsoneditor-menu {
-    background-color: ${COLOR_GREY_LIGHT_2};
-    border-bottom: 0;
-    border-radius: 4px 4px 0 0;
-    & > button {
-      color: ${COLOR_PRIMARY};
-    }
-  }
   .jsoneditor {
-    border: 1px solid ${COLOR_GREY_LIGHT_2};
+    border: 0;
     border-radius: 4px;
+  }
+  .ace_content {
+    background-color: ${COLOR_GREY_LIGHT_1};
   }
   .ace_editor {
     font-family: 'Source Code Pro', sans-serif;
@@ -43,10 +38,36 @@ const StyledEditor = styled.div`
   .jsoneditor-text {
     background-color: ${COLOR_GREY_LIGHT_1};
   }
-  .jsoneditor-statusbar {
-    border-radius: 0 0 4px 4px;
+`;
+
+const EditorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  flex-shrink: 1;
+  position: relative;
+`;
+
+const DragHoverText = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(255, 255, 255, 0.7);
+  z-index: 1010;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  > p {
+    text-align: center;
   }
 `;
+
+const overrideEventDefaults = event => {
+  event.preventDefault();
+  event.stopPropagation();
+};
 
 class ConfigurationModal extends React.Component {
   constructor(props) {
@@ -54,8 +75,86 @@ class ConfigurationModal extends React.Component {
     this.state = {
       config: props.configuration,
     };
+    this.dragEventCounter = 0;
+
+    this.downloadConfig = this.downloadConfig.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.dragenterListener = this.dragenterListener.bind(this);
+    this.dragleaveListener = this.dragleaveListener.bind(this);
+    this.dropListener = this.dropListener.bind(this);
+  }
+
+  dragenterListener(event) {
+    overrideEventDefaults(event);
+    this.dragEventCounter++;
+    if (event.dataTransfer.items && event.dataTransfer.items[0]) {
+      this.setState({ dragging: true });
+    }
+  }
+
+  dragleaveListener(event) {
+    overrideEventDefaults(event);
+    this.dragEventCounter--;
+
+    if (this.dragEventCounter === 0) {
+      this.setState({ dragging: false });
+    }
+  }
+
+  dropListener(event) {
+    overrideEventDefaults(event);
+    this.dragEventCounter = 0;
+    this.setState({ dragging: false });
+
+    if (event.dataTransfer.files && event.dataTransfer.files[0]) {
+      const f = event.dataTransfer.files[0];
+      const reader = new FileReader();
+
+      reader.onload = e => {
+        try {
+          const config = JSON.parse(e.target.result);
+          this.handleChange(config);
+        } catch (error) {
+          // TODO: Add notifications. Show notification for invalid json
+          console.log(error);
+        }
+      };
+      reader.readAsText(f);
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('dragover', event => {
+      overrideEventDefaults(event);
+    });
+    window.addEventListener('drop', event => {
+      overrideEventDefaults(event);
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('dragover', overrideEventDefaults);
+    window.removeEventListener('drop', overrideEventDefaults);
+  }
+
+  downloadConfig() {
+    const { config } = this.state;
+    const element = document.createElement('a');
+    element.setAttribute(
+      'href',
+      `data:text/json;charset=utf-8,${encodeURIComponent(
+        JSON.stringify(config, null, 2),
+      )}`,
+    );
+    element.setAttribute('download', 'zethus_config.json');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
   }
 
   handleChange(config) {
@@ -72,26 +171,47 @@ class ConfigurationModal extends React.Component {
 
   render() {
     const { closeModal } = this.props;
-    const { config } = this.state;
+    const { config, dragging } = this.state;
 
     return (
       <ModalWrapper onClick={closeModal}>
         <ModalContents onClick={stopPropagation}>
           <ModalTitle>Edit Configuration</ModalTitle>
-          <Editor
-            ace={ace}
-            mode="code"
-            value={config}
-            onChange={this.handleChange}
-            tag={StyledEditor}
-            theme="ace/theme/xcode"
-          />
+          <EditorWrapper
+            onDrag={overrideEventDefaults}
+            onDragStart={overrideEventDefaults}
+            onDragEnd={overrideEventDefaults}
+            onDragOver={overrideEventDefaults}
+            onDragEnter={this.dragenterListener}
+            onDragLeave={this.dragleaveListener}
+            onDrop={this.dropListener}
+          >
+            <Editor
+              ace={ace}
+              mode="code"
+              value={config}
+              onChange={this.handleChange}
+              tag={StyledEditor}
+              theme="ace/theme/xcode"
+              mainMenuBar={false}
+              statusBar={false}
+              htmlElementProps={{}}
+            />
+            {dragging && (
+              <DragHoverText>
+                <p>Drag & drop Configuration JSON file</p>
+              </DragHoverText>
+            )}
+          </EditorWrapper>
           <ModalActions>
+            <ButtonOutline onClick={this.downloadConfig}>
+              Download
+            </ButtonOutline>
             <FlexGrow />
             <ButtonPrimary onClick={this.handleSubmit}>
               Update Configuration
             </ButtonPrimary>
-            <ButtonPrimary onClick={closeModal}>Cancel</ButtonPrimary>
+            <ButtonOutline onClick={closeModal}>Cancel</ButtonOutline>
           </ModalActions>
         </ModalContents>
       </ModalWrapper>
